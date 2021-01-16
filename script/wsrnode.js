c76d10bb8d27f1ab4b7502a1472b777cec521708
LaurentJeannel <jeannellaurent11@gmail.com>
2021-01-16 18:31:54 +0100

    n
    n

--- script/wsrnode.js
+++ script/wsrnode.js
@@ -1,6 +1,6 @@
 try{
 
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 //https://drive.google.com/drive/u/0/folders/1wlS25fvVdtspTrcLyt4B2IaPQrKXwtXo
 var requesta = require('request');
 var fs = require('fs');
